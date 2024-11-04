6a28434d38f961b2a33aead0a7a474433f8e9b3b
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2024-11-04 14:20:15 +0200

    initial commit

---
+++ backend/Dotcafe/src/main/java/com/example/Dotcafe/DotcafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dotcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotcafeApplication.class, args);
+	}
+
+}
---
+++ backend/Dotcafe/src/test/java/com/example/Dotcafe/DotcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dotcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
