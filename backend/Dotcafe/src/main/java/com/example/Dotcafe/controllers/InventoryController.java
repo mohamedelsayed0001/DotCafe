e809657781232b696fe93077af3de6db187881d4
Samayosri <samayosri2004@gmail.com>
2024-12-09 17:48:52 +0200

    item

---
+++ backend/Dotcafe/src/main/java/com/example/Dotcafe/controllers/InventoryController.java
@@ -0,0 +1,52 @@
+package com.example.Dotcafe.controllers;
+
+import com.example.Dotcafe.entity.Dto.ItemDto;
+import com.example.Dotcafe.entity.Item;
+import com.example.Dotcafe.sevices.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Admin/inventory")
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+    @PostMapping ("/createitem")
+    public ResponseEntity<?>create(@RequestBody ItemDto itemDto) {
+        try{
+            return new ResponseEntity<>(inventoryService.createitem(itemDto), HttpStatus.CREATED);
+        } catch (CloneNotSupportedException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("item already exists");
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @PutMapping("/edititem")
+    public ResponseEntity<?>edit(@RequestBody ItemDto itemDto) {
+        try{
+            return new ResponseEntity<>(inventoryService.edititem(itemDto), HttpStatus.CREATED);
+        }  catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Item name already exists");
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @DeleteMapping("/deleteitem")
+    public ResponseEntity<?> delete(@RequestBody ItemDto itemDto) {
+        try {
+            ItemDto deleteditem = inventoryService.deleteitem(itemDto);
+            return new ResponseEntity<>(deleteditem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ backend/Dotcafe/src/main/java/com/example/Dotcafe/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.example.Dotcafe.repository;
+
+import com.example.Dotcafe.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> getitembyname(String name);
+
+
+}
