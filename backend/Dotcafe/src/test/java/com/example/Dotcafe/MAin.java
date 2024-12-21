e37f20c85c53a428cac0fb1d5648f4077400e2e1
Nour-Eldien <noureldientaha@gmail.com>
2024-12-21 13:59:07 +0200

    added users table

--- backend/Dotcafe/src/test/java/com/example/Dotcafe/MAin.java
+++ backend/Dotcafe/src/test/java/com/example/Dotcafe/MAin.java
@@ -1,23 +1,23 @@
-package com.example.Dotcafe.sevices;
+// package com.example.Dotcafe.sevices;
 
-import org.junit.jupiter.api.Test;
-import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.stereotype.Service;
-
-
-public class MAin {
-
-   static String encodePassword(String rawPassword) {
-
-        return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
-    }
-
-    public static void main(String[] args) {
+// import org.junit.jupiter.api.Test;
+// import org.mindrot.jbcrypt.BCrypt;
+// import org.springframework.stereotype.Service;
 
 
-        System.out.println(encodePassword("1234"));
-    }
+// public class MAin {
+
+//    static String encodePassword(String rawPassword) {
+
+//         return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
+//     }
+
+//     public static void main(String[] args) {
+
+
+//         System.out.println(encodePassword("1234"));
+//     }
 
 
 
-}
+// }
